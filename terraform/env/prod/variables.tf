9aebe5ac926ae819d9b7e1ef0c28178cc6316f48
ayush <ayushkamble820@gmail.com>
2026-08-23 03:04:08 +0530

    terraform/env/dev and terraform/env/prod updated

--- terraform/env/prod/variables.tf
+++ terraform/env/prod/variables.tf
@@ -1,4 +1,17 @@
-variable "aws_region"          { type = string; default = "us-east-1" }
-variable "domain_name"         { type = string }
-variable "acm_certificate_arn" { type = string }
-variable "alb_dns_name"        { type = string; default = "" }
+variable "aws_region" {
+  type    = string
+  default = "eu-north-1"
+}
+
+variable "domain_name" {
+  type = string
+}
+
+variable "acm_certificate_arn" {
+  type = string
+}
+
+variable "alb_dns_name" {
+  type    = string
+  default = ""
+}
